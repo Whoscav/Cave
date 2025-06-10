dd40d5eefe86dc5d14d0a5cd298648868ee5ae14
Whoscav <raphaella.2023305474@aluno.iffar.edu.br>
2025-06-10 09:15:31 -0300

    Update rapha.php

--- rapha.php
+++ rapha.php
@@ -1,1 +1,61 @@
+<?php
 
+class Caneta {
+
+public $marca;
+
+private $cor;
+
+private Sponta;
+
+public function getPonta() {
+
+return $this->ponta;
+
+}
+
+public function getcor() {
+
+return $this->cor;
+
+}
+
+public function getmarca() {
+
+return $this->marca;
+
+}
+
+public function setPonta ($ponta) {
+
+$this->ponta = $ponta;
+
+}
+
+public function setcor($cor) {
+
+$this->cor = $cor;
+
+}
+
+public function setmarca ($marca) {
+
+$this->marca = $marca;
+
+}
+
+public function _ construct() {
+
+$this->ponta = 0.5;
+
+}
+
+}
+
+$c1 = new Caneta();
+
+$c2= new Caneta();
+
+print_r($c1);
+
+print_r($c2);
